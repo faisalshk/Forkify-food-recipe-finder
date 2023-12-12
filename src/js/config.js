6265a5a935078ea687413607dc0f3d96e83875ab
faisalshk <86406064+faisalshk@users.noreply.github.com>
2023-12-12 18:25:25 +0530

    Project complete

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bec99bd3-0b29-4d0c-a2e3-dcf9647d4b49';
+export const MODAL_CLOSE_SEC = 2.5;
